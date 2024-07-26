5e45e9d9c9c7b39e62e8a4208e00442087fa40b2
Alexander Nørgaard <alexander@Alexanders-MacBook-Pro.local>
2024-07-26 17:14:25 +0200

    Models working with migrations

--- cmd/model/event.go
+++ cmd/model/event.go
@@ -8,18 +8,18 @@ import (
 
 type Event struct {
 	Model
-	Title         string       `json:"title" gorm:"not null"`
-	Description   string       `json:"description" gorm:"default:NULL"`
-	Owners        []User       `json:"owner" gorm:"many2many:event_owners"`
-	Private_event bool         `json:"private_event" gorm:"default:false"`
-	Time_start    time.Time    `json:"time_start"`
-	Time_end      time.Time    `json:"time_end" gorm:"default:NULL;check:time_end > time_start"`
-	Address       geo.Address  `json:"address"`
-	Geolocation   geo.Location `json:"geolocation" gorm:"default:NULL"`
+	Title         string        `json:"title" gorm:"not null"`
+	Description   string        `json:"description" gorm:"default:NULL"`
+	Owners        []User        `json:"owner" gorm:"many2many:event_owners"`
+	Private_event bool          `json:"private_event" gorm:"default:false"`
+	Time_start    time.Time     `json:"time_start"`
+	Time_end      time.Time     `json:"time_end" gorm:"default:NULL;check:time_end > time_start"`
+	Address       *geo.Address  `json:"address" gorm:"embedded"`
+	Geolocation   *geo.Location `json:"geolocation" gorm:"embedded"`
 }
 
 type Subscription struct {
 	Model
-	Event Event `gorm:"not null"`
-	User  User  `gorm:"not null"`
+	Event Event `gorm:"embedded;not null"`
+	User  User  `gorm:"embedded;not null"`
 }

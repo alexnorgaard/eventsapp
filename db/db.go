c0b131d0fb4ed9234501e10dd550adc58e2eafa3
Alexander Nørgaard <alexander@Alexanders-MacBook-Pro.local>
2024-07-27 15:48:12 +0200

    Geolocation client implemented

--- db/db.go
+++ db/db.go
@@ -1,14 +1,14 @@
 package db
 
 import (
-	cofig "github.com/alexnorgaard/eventsapp"
+	config "github.com/alexnorgaard/eventsapp"
 	"github.com/alexnorgaard/eventsapp/cmd/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connect() (*gorm.DB, error) {
-	conf := cofig.GetConf()
+	conf := config.GetConf()
 	dsn := "host=" + conf.Postgres.Host + " user=" + conf.Postgres.User + " password=" + conf.Postgres.Password + " dbname=" + conf.Postgres.Database + " port=" + conf.Postgres.Port + " sslmode=disable TimeZone=Europe/Copenhagen"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	return db, err
---
+++ internal/geolocation_client/client.go
@@ -0,0 +1,35 @@
+package geolocationclient
+
+import (
+	"errors"
+
+	config "github.com/alexnorgaard/eventsapp"
+	"github.com/codingsince1985/geo-golang"
+	"github.com/codingsince1985/geo-golang/google"
+)
+
+func GetGeolocation(address string) (*geo.Location, error) {
+	api_key := config.GetConf().Google_geocoding_api.Api_key
+	geocoder := google.Geocoder(api_key)
+	location, err := geocoder.Geocode(address)
+	if err != nil {
+		return nil, err
+	}
+	if location == nil {
+		return nil, errors.New("No location found")
+	}
+	return location, nil
+}
+
+func GetAddress(lat float64, lng float64) (*geo.Address, error) {
+	api_key := config.GetConf().Google_geocoding_api.Api_key
+	geocoder := google.Geocoder(api_key)
+	address, err := geocoder.ReverseGeocode(lat, lng)
+	if err != nil {
+		return nil, err
+	}
+	if address == nil {
+		return nil, errors.New("No address found")
+	}
+	return address, nil
+}

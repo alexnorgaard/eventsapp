757bb6f0e3b8d9b3db4ae2343fae97645c3fb1ca
Alexander Arlund Nørgaard <alexandera@moneyflow.io>
2024-09-12 16:42:36 +0200

    Check for UUIDv4 instead of just UUID on validaotr

--- cmd/model/model.go
+++ cmd/model/model.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Model struct {
-	ID        uuid.UUID    `gorm:"<-:create;type:uuid;primary_key;default:uuid_generate_v4()" param:"id" validate:"omitempty,uuid"`
+	ID        uuid.UUID    `gorm:"<-:create;type:uuid;primary_key;default:uuid_generate_v4()" param:"id" validate:"omitempty,uuid4"`
 	CreatedAt time.Time    `gorm:"autoCreateTime:milli"`
 	UpdatedAt time.Time    `gorm:"autoUpdateTime:milli"`
 	DeletedAt sql.NullTime `gorm:"index"`

67df3730d7419f92926bb64bfe781d26fcffbd3b
Alexander Arlund Nørgaard <alexandera@moneyflow.io>
2024-09-27 13:40:39 +0200

    Remove TLS connection in echo as it's handled by NGINX - Certificate renewal also removed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"crypto/tls"
-	"fmt"
-	"net/http"
-
 	"github.com/alexnorgaard/eventsapp/cmd/handler"
 	router "github.com/alexnorgaard/eventsapp/cmd/router"
 	dbmodule "github.com/alexnorgaard/eventsapp/db"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/crypto/acme"
-	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -24,27 +18,26 @@ func main() {
 	es := handler.NewEventStore(db)
 	h := handler.NewHandler(es)
 	router.RegisterRoutes(e, h)
-	autoTLSManager := autocert.Manager{
-		Prompt: autocert.AcceptTOS,
-		// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
-		Cache:      autocert.DirCache("/usr/local/nginx/conf"),
-		HostPolicy: autocert.HostWhitelist("app.alexnorgaard.dk"),
-	}
+	// autoTLSManager := autocert.Manager{
+	// 	Prompt: autocert.AcceptTOS,
+	// 	// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+	// 	Cache:      autocert.DirCache("/usr/local/nginx/conf"),
+	// 	HostPolicy: autocert.HostWhitelist("app.alexnorgaard.dk"),
+	// }
 
-	s := http.Server{
-		Addr:    ":https",
-		Handler: e, // set Echo as handler
-		TLSConfig: &tls.Config{
-			//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
-			GetCertificate: autoTLSManager.GetCertificate,
-			NextProtos:     []string{acme.ALPNProto},
-		},
-		//ReadTimeout: 30 * time.Second, // use custom timeouts
-	}
-	fmt.Println("starting listen and serve")
-	if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
-		e.Logger.Fatal(err)
-	}
-
-	// e.Logger.Fatal(e.Start(":1323"))
+	// s := http.Server{
+	// 	Addr:    ":https",
+	// 	Handler: e, // set Echo as handler
+	// 	TLSConfig: &tls.Config{
+	// 		//Certificates: nil, // <-- s.ListenAndServeTLS will populate this field
+	// 		GetCertificate: autoTLSManager.GetCertificate,
+	// 		NextProtos:     []string{acme.ALPNProto},
+	// 	},
+	// 	//ReadTimeout: 30 * time.Second, // use custom timeouts
+	// }
+	// fmt.Println("starting listen and serve")
+	// if err := s.ListenAndServeTLS("", ""); err != http.ErrServerClosed {
+	// 	e.Logger.Fatal(err)
+	// }
+	e.Logger.Fatal(e.Start(":8080"))
 }
